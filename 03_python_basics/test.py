6f13120e5d2c7f4a81f8a97f43d9d304e9957cfe
Kapil <kapil0123@gmail.com>
2026-03-14 21:54:34 +0530

    added 03 and 04 codes

---
+++ 03_python_basics/test.py
@@ -0,0 +1,8 @@
+## Indentation
+## Python uses indentation to define blocks of code. Consistent use of spaces (commonly 4) or a tab is required.
+
+age=25
+
+if age>30:              # 25 > 30 false
+    print("the age is",age)          # if true execute this line
+print("outside the if block the age is: ",age)              # this will also get executed
